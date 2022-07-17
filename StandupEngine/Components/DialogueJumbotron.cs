7a2738e022faeb179fb10f9907ad58e4e0cf28f6
mariazieba <89458930+mariazieba@users.noreply.github.com>
2022-07-17 23:31:31 +0200

    new background, fixed styling to match

--- StandupEngine/Components/DialogueJumbotron.cs
+++ StandupEngine/Components/DialogueJumbotron.cs
@@ -5,9 +5,6 @@ namespace StandupEngine.Components
 {
     public partial class DialogueJumbotron : ComponentBase
     {
-        /*
-         Music from <a href="https://pixabay.com/music/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=music&amp;utm_content=38826">Pixabay</a>
-        */
         [Inject]
         public Engine Engine { get; set; }
         [CascadingParameter]
